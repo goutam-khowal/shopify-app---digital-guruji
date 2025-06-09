1a40223abacb21c9d1fc19b5248aaccd34293d63
GoutamK1902 <goutamkhowal321@gmail.com>
2025-06-09 19:36:29 +0530

    vercel

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,4 @@
+const { vercelPreset } = require("remix-vite");
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
 // stop passing in HOST, so we can remove this workaround after the next major release.
@@ -17,4 +18,5 @@ module.exports = {
   serverModuleFormat: "cjs",
   dev: { port: process.env.HMR_SERVER_PORT || 8002 },
   future: {},
+  ...vercelPreset(),
 };
